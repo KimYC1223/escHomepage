8e8d7cb64ff3bd766c1d0467492351daeb0b313c
KimYC1223 <kau_esc@naver.com>
2019-10-19 19:01:30 +0900

    complete header 2

--- HTML_CSS_JS/HeaderImg.js
+++ HTML_CSS_JS/HeaderImg.js
@@ -3,8 +3,8 @@ let HeaderImg = [ '<div class="slide start"><img id="header001" src="./img/heade
                   '<div class="slide"><img id="header002" src="./img/header/header002.jpg" /></div>',
                   '<div class="slide"><img id="header003" src="./img/header/header003.jpg" /></div>']
 
-let HeaderImgBtn = ['<input type="radio" name="r" id="r1" checked>',
-                    '<input type="radio" name="r" id="r2">',
-                    '<input type="radio" name="r" id="r3">']
+let HeaderImgBtn = ['<label for="r1" class="buttonDiv"></label>',
+                    '<label for="r2" class="buttonDiv"></label>',
+                    '<label for="r3" class="buttonDiv"></label>']
 
 let HeaderText = '홈페이지테스트!'
--- HTML_CSS_JS/index.js
+++ HTML_CSS_JS/index.js
@@ -4,7 +4,12 @@ $(document).ready(function(){
       str = str + HeaderImg[i] + '\n'
     }
 
+    let str2 = ''
+    for (let i = 0 ; i < HeaderImgBtn.length ; i ++){
+      str2 = str2 + HeaderImgBtn[i] + '\n'
+    }
 
     document.getElementById("headerImgDiv").innerHTML= document.getElementById("headerImgDiv").innerHTML+str;
+    document.getElementById("headerSelect").innerHTML=str2
     document.getElementById("headerImgText").innerHTML=HeaderText
 })
