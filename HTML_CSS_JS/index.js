d92f744ef51b366e416d4fc3958323cb4cedd064
KimYC1223 <kau_esc@naver.com>
2019-10-23 02:12:25 +0900

    MainPage All Layout Complete

--- HTML_CSS_JS/index.js
+++ HTML_CSS_JS/index.js
@@ -1,3 +1,7 @@
+let gotoNaver = () => {
+  location.href='http://naver.me/Fppgb8MJ'
+}
+
 $(document).ready(function(){
     let str = ''
     for (let i = 0 ; i < HeaderImg.length ; i ++){
@@ -9,7 +13,26 @@ $(document).ready(function(){
       str2 = str2 + HeaderImgBtn[i] + '\n'
     }
 
-    document.getElementById("headerImgDiv").innerHTML= document.getElementById("headerImgDiv").innerHTML+str;
+    document.getElementById("headerImgDiv").innerHTML=
+      document.getElementById("headerImgDiv").innerHTML+str
+
     document.getElementById("headerSelect").innerHTML=str2
     document.getElementById("headerImgText").innerHTML=HeaderText
+
+
+    //============================================
+    // For MainPage
+    //============================================
+    let currentTimeSpan = document.getElementById('currentTimeSpan')
+    if (currentTimeSpan != null){
+      let date = new Date()
+      let currentTimeStr = '기준 시간 : '
+      currentTimeStr += date.getFullYear() + '-'
+      currentTimeStr += (date.getMonth()+1) + '-'
+      currentTimeStr += date.getDate() + ' '
+      currentTimeStr += date.getHours() + ':'
+      currentTimeStr += date.getMinutes() + ':'
+      currentTimeStr += date.getSeconds()
+      currentTimeSpan.innerHTML = currentTimeStr;
+    }
 })
